c9630911e3620c7d9d3b39df5bdec79a4764f29e
Evan Kaplan <evnkpln@gmail.com>
2017-04-27 23:12:23 +0000

    Assignment 8: Adds demloish function

--- controllers/menu_controller.rb
+++ controllers/menu_controller.rb
@@ -14,6 +14,7 @@ class MenuController
         puts "3 - Search for an entry"
         puts "4 - Import entries from a CSV"
         puts "5 - Exit"
+        puts "6 - Delete all entries"
         print "Enter your selection: "
         
         selection = gets.to_i
@@ -38,6 +39,10 @@ class MenuController
             when 5
                 puts "Good-bye!"
                 exit(0)
+            when 6
+                system "clear"
+                demolish
+                main_menu
             else
                 system "clear"
                 puts "Sorry, that is not a valid input"
@@ -178,6 +183,12 @@ class MenuController
         
         puts "Updated entry:"
         puts entry
+    end
+    
+    def demolish
+        until address_book.entries.empty?
+            delete_entry(address_book.entries[0])
+        end
     end
     
 end
